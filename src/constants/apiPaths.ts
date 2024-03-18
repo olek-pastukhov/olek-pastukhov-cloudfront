5b0e8c6317b1fa10ac219d3abe24a8b942d19363
Oleksandr Pastukhov <flashka.ua@gmail.com>
2024-03-18 19:37:32 +0100

    OAC based yaml
    optimized OAC based yaml

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://olek-shop-app.s3-website-eu-west-1.amazonaws.com/dev",
+  order: "https://olek-shop-app.s3-website-eu-west-1.amazonaws.com/dev",
+  import: "https://olek-shop-app.s3-website-eu-west-1.amazonaws.com/dev",
+  bff: "https://olek-shop-app.s3-website-eu-west-1.amazonaws.com/dev",
+  cart: "https://olek-shop-app.s3-website-eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
